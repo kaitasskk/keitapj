137fa96280046e0f965f4a0b3dbd1e5d0709c100
ShimuraKeita <keitagsk8001@gmail.com>
2020-07-06 07:15:35 +0900

    さしすせそ　追加

--- keitapj/ViewController.swift
+++ keitapj/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("あいうえお")
+        
+        print("さしすせそ")
         // Do any additional setup after loading the view.
     }
 
