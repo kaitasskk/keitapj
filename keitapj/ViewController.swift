bc16a48eddd4aa7889c44f1fdf6decf25e47d4a1
ShimuraKeita <keitagsk8001@gmail.com>
2020-07-06 07:04:54 +0900

    Initial Commit

---
+++ keitapj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  keitapj
+//
+//  Created by 志村　啓太 on 2020/07/06.
+//  Copyright © 2020 Keita Shimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
